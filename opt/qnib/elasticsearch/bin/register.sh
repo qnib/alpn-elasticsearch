74d7de30264f03f3635da94e5a235fc89fb103d8
Christian Kniep <ckniep@gaikai.com>
2016-08-23 10:05:48 +0200

    register index settings/mappings

--- opt/qnib/elasticsearch/bin/register.sh
+++ opt/qnib/elasticsearch/bin/register.sh
@@ -3,14 +3,22 @@ set -x
 
 source /opt/qnib/consul/etc/bash_functions.sh
 
-wait_for_srv elasticsearch 
+wait_for_srv elasticsearch
 
 set -e
 
-##### Register index templates #####
+##### Register index settings #####
+for x in $(find /opt/qnib/elasticsearch/index-registration/settings/ -type f -name "*.json");do
+    IDX=$(echo ${x} |awk -F/ '{print $NF}' |awk -F. '{print $1}')
+    echo "> PUT '${x}' into '_template/set_${IDX}'"
+    curl -sXPUT "http://127.0.0.1:9200/_template/set_${IDX}" --data-binary @${x}
+done
+
+##### Register index mappings #####
 for x in $(find /opt/qnib/elasticsearch/index-registration/mappings/ -type f -name "*.json");do
     IDX=$(echo ${x} |awk -F/ '{print $NF}' |awk -F. '{print $1}')
-    curl -sXPUT "http://127.0.0.1:9200/_template/${IDX}" --data-binary @${x}
+    echo "> PUT '${x}' into '_template/map_${IDX}'"
+    curl -sXPUT "http://127.0.0.1:9200/_template/map_${IDX}" --data-binary @${x}
 done
 
 exit 0
